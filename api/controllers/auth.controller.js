a6fbee2694a9ba94be6911973fd9d6b8c4c60411
rajaabdullah833 <rajasofiyan2017@gmail.com>
2024-09-16 17:58:56 +0500

    update the header

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
@@ -6,7 +7,7 @@ import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
+    const newUser = new User({ username, email, password: hashedPassword});
     try {
       await newUser.save();
       res.status(201).json('User created successfully!');
