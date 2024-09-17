69a3300071e59ef3f2e31c6b417ffb5090d5f4ca
rajaabdullah833 <rajasofiyan2017@gmail.com>
2024-09-17 19:40:17 +0500

    create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,43 @@
-export const test = (req, res)=>{
-    res.json({
-        message: 'Api route is working'
-    });
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
+export const test = (req, res) => {
+  res.json({
+    message: 'API route is working',
+  });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next({ status: 401, message: 'You can only update your own account!' });
+  }
+  
+  try {
+    // Hash password if provided
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    // Find and update the user
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    // Exclude the password from the response
+    const { password, ...rest } = updatedUser._doc;
+
+    // Send the updated user data
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
