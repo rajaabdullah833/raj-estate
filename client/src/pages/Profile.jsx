ad0e8246eeb24b7f98f027b05748d5c9406f7c95
rajaabdullah833 <rajasofiyan2017@gmail.com>
2024-10-14 21:26:50 +0500

    complete delete user listing funtionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -145,6 +145,24 @@ export default function Profile() {
     } catch (error) {
       setShowListingsError(true);
     }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(error.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+
+    } catch (error) {
+      console.log(error.message);
+    }
   }
   return (
     <div className="p-3 max-w-lg mx-auto flex flex-col items-center">
